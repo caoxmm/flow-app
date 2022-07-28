4055a334b45f4bb7b1175d2be6eb3a2b2d164a19
caoxmm <386707656@qq.com>
2022-07-28 20:28:01 +0800

    add demo

---
+++ flow/config.js
@@ -0,0 +1,8 @@
+import { config } from "@onflow/fcl";
+
+config({
+  "accessNode.api": "https://rest-testnet.onflow.org", // Mainnet: "https://rest-mainnet.onflow.org"
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn", // Mainnet: "https://fcl-discovery.onflow.org/authn"
+  "0xNON_FUNGIBLE_TOKEN_ADDRESS": "0x631e88ae7f1d7c20",
+  "0xVOUCHER_ADDRESS": "0x7f3812b53dd4de20",
+});
